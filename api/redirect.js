5ed06c3a3bf22f60b9b8af44404be68a5ff5b605
Umanandnaik2003 <gnvun.uma@gmail.com>
2025-04-02 11:50:38 +0530

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,3 +1,9 @@
-modules.export = (req, res) => {
-    res.status(200).json({message :"hello world"});
-}
+module.exports = (req, res) => {
+    try {
+      // Perform the redirect
+      res.redirect(301, 'https://maps.app.goo.gl/W5L9FX6z11KrE7fH9');
+    } catch (error) {
+      console.error("Error during redirect:", error);
+      res.status(500).send("Internal Server Error");
+    }
+};
