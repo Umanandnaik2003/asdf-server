6c978d3b1e10deddba8a62f0c4ea56db3ad6bb5f
Umanandnaik2003 <gnvun.uma@gmail.com>
2025-04-02 11:59:36 +0530

    meta pixel

--- api/redirect.js
+++ api/redirect.js
@@ -1,3 +1,23 @@
-modules.export = (req, res) => {
-    res.status(200).json({message :"hello world"});
-}
+module.exports = (req, res) => {
+  try {
+    res.send(`<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '9367577546693166');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=9367577546693166&ev=PageView&noscript=1"
+/></noscript>
+`);
+  } catch (error) {
+    console.error("Error during redirect:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
